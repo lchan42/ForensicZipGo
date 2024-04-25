e645a3372c6136180c478cc2bfa36c72dc642cb4
Luc Chan <luc92.chan@gmail.com>
2024-04-25 19:55:56 +0200

    feat: implementing flags parsing
    - user can use -adp -ha -z to specify path to artifactDefinition.yaml, hash algo and zip. Default value added for adp and ha, z is mandatory
    
    to do next : link with parser

--- cmd/forensiczip/main.go
+++ cmd/forensiczip/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"forensic-zip-tool/internal"
+	// "forensic-zip-tool/internal"
 	"log"
 	"fmt"
-	// "flag"
+	"flag"
 )
 
 func handleError(err error, customMessage string) {
@@ -14,11 +14,19 @@ func handleError(err error, customMessage string) {
 }
 
 func main () {
+	//parse commande line using flags. Flag -adp, -ha and -z stand respectively for artefact definition path, hash algorithm and zip 
+	definitionPath := flag.String("adp", "../../configs/windowsArtifact.yaml", "[Optional] path to the artefact_definition.yaml")
+	hashAlg := flag.String("ha", "sha256", "[Optional] Digest algorithm to use. default sha256")
+    // archivePath := flag.String("z", "", "[Mandatory] Path to store the final ZIP archive")
 
-	// definitionPath := flag.String("definitionPath", )
-	result, errParser := internal.ArtifactParser("/home/lchan/Documents/formations/projet_go/ForensicZipGo/test_directory/windows.yaml", "FILE")
-	handleError(errParser, "Error while Parsing file\n")
+	flag.Parse()
 
-	fmt.Print(result)
+	// result, errParser := internal.ArtifactParser("/home/lchan/Documents/formations/projet_go/ForensicZipGo/test_directory/windows.yaml", "FILE")
+	// handleError(errParser, "Parsing file\n")
+
+	// fmt.Print(result)
+
+	fmt.Printf("%v\n", *definitionPath)
+	fmt.Printf("%v\n", *hashAlg)
 }
 
