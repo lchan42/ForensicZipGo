0422499a5f3ecdef2b7b283656b6f58b84c8fbff
Luc Chan <luc92.chan@gmail.com>
2024-04-25 18:33:34 +0200

    feat: adding name parser
    - pk/internal -> ArtifactParser(path, typeToparse)
    
    do to next: retrive args

--- cmd/forensiczip/main.go
+++ cmd/forensiczip/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main () {
+import (
+	"forensic-zip-tool/internal"
+	"log"
+	"fmt"
+)
 
+func handleError(err error, customMessage string) {
+	if err != nil {
+		log.Fatalf("Error %s: %s\n", customMessage, err)
+	}
+}
+
+
+func main () {
+	result, errParser := internal.ArtifactParser("/home/lchan/Documents/formations/projet_go/ForensicZipGo/test_directory/windows.yaml", "FILE")
+	handleError(errParser, "Error while Parsing file\n")
+
+	fmt.Print(result)
 }
